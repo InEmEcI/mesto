f1977d686e76d83e67b9b446a73aff47b8b5ef9b
InEmEcI <iikar@bk.ru>
2023-01-29 23:26:26 +0300

    Можно добавлять карточки

--- scripts/index.js
+++ scripts/index.js
@@ -3,23 +3,67 @@
 // кнопка like
 const likeButton = document.querySelector('.element__like');
 
+// попап
 const popupElement = document.querySelector('.popup');
-const profileOpenButton = document.querySelector('.profile__edit');
-const popupCloseElement = popupElement.querySelector('.popup__close');
-const profileElement = document.querySelector('.profile');
-let profileName = profileElement.querySelector('.profile__name');
-let profileWhoIsThisElement = profileElement.querySelector('.profile__who-is-this');
-let popupNameElement = popupElement.querySelector('.popup__name');
-let popupWhoIsThisElement = popupElement.querySelector('.popup__who-is-this');
-const popupContentSaveButton = popupElement.querySelector('.popup__save');
-const popupAddNewCardSaveButton = popupElement.querySelector('.new-card-save');
-const formElement = document.querySelector('.popup__form-about');
-let nameInput = formElement.querySelector('.popup__input_name_name');
-let jobInput = formElement.querySelector('.popup__input_name_who-is-this');
 
+// кнопка открытия попапа в профиле
+const profileOpenButton = document.querySelector('.profile__edit');
+
+// кнопка закрытия попапа в профиле
+const popupCloseElement = popupElement.querySelector('.popup__close');
+
+// профиль
+const profileElement = document.querySelector('.profile');
+
+// как зовут (в профиле)
+let profileName = profileElement.querySelector('.profile__name');
+
+// кто он (в профиле)
+let profileWhoIsThisElement = profileElement.querySelector('.profile__who-is-this');
+
+// как зовут (в попапе)
+let popupNameElement = popupElement.querySelector('.popup__name');
+
+// кто он (в попапе)
+let popupWhoIsThisElement = popupElement.querySelector('.popup__who-is-this');
+
+// кнопка сохранения информации после редактирования в попапе профиля
+const popupContentSaveButton = popupElement.querySelector('.popup__save');
+
+// popup_new-place-form
+const popupNewPlaceForm = document.querySelector('.popup_new-place-form');
+
+// попап новой карточки  popup-new-card
+const popupNewCardItem = document.querySelector('.popup_new-card');
+
+// имя карточки а попапе
+const newCardName = document.querySelector('.popup__input_type_card_name')
+// ссылка на картинку карточки а попапе
+const newCardLink = document.querySelector('.popup__input_type_photo-link')
+
+// кнопка сохранения информации в попапе при создании новой карточки
+const popupAddNewCardSaveButton = popupNewCardItem.querySelector('.new-card-save');
+
+// кнопка открытия попапа новой карточки
+const popupAddNewCardOpen = document.querySelector('.profile__add-new')
+
+// кнопка закрытия попапа новой карточки
+const popupAddNewCardCloseButton = popupNewCardItem.querySelector('.popup-new-card_close');
+
+// форма в попапе профиля
+const formElement = document.querySelector('.popup__form-about');
+
+// имя в попапе профиля
+const nameInput = formElement.querySelector('.popup__input_name_name');
+// "кто он" в попапе профиля
+const jobInput = formElement.querySelector('.popup__input_name_who-is-this');
+
+// закрытие попапа
+const closes = document.querySelectorAll('.popup__close');
 
 // Карточки с городами
 const cardsSection = document.querySelector('.elements');
+
 
 // Карточка
 // const cardsSectionElement = cardsSection.querySelector('.element');
@@ -30,13 +74,29 @@ const cardsSection = document.querySelector('.elements');
 
 // карточка с городами в template(шаблоне) (<article class="element">)
 const templateCard = document.querySelector('#template-card').content.querySelector('.element');
-// console.log(templateCard);
 
 // кнопка добавления карточки
 const newCardButton = document.querySelector('.profile__add-new');
 
 // кнопка удаления карточки
 const elementTrashButton = cardsSection.querySelector('.element__trash');
+
+// закрытие попапа
+const popupClose = function (evt) {
+  evt.target.closest('.popup').classList.remove('popup_opened');
+}
+
+// открытие попапа в профиле
+const popupOpen = function () {
+  popupElement.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileWhoIsThisElement.textContent;
+}
+
+// открытие попапа
+const openAddNewCardPopup = function(){
+  popupNewCardItem.classList.add('popup_opened');
+}
 
 // функция для добавления карточки
 function createCard(items) {
@@ -58,31 +118,34 @@ function createCard(items) {
 // вставляем карточки из массива initialCards[]
 createCard(initialCards);
 
-// createCard(newCard);
-
-// -------------- УЖЕ БЫЛО ---------------------------
-
-// открытие попапа в профиле
-const popupOpen = function () {
-  popupElement.classList.add('popup_opened');
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileWhoIsThisElement.textContent;
-}
-
-// закрытие попапа в профиле
-const popupClose = function () {
-  popupElement.classList.remove('popup_opened');
-}
+// вешаем события "закрытие попапа" на каждый крестик
+closes.forEach( (closeButton) => {
+  closeButton.addEventListener('click', popupClose);
+})
 
 // заполнение полей в профиле
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileWhoIsThisElement.textContent = jobInput.value;
-  popupClose();
+  popupClose(evt);
 }
 
+// функция добавления новой карточки
+function addNewCard (evt) {
+  evt.preventDefault();
+  const newCard = [{name: newCardName.value, link: newCardLink.value,}]
+  createCard(newCard);
+  popupClose(evt);
+}
+
+
 profileOpenButton.addEventListener('click', popupOpen);
-popupCloseElement.addEventListener('click', popupClose);
+popupAddNewCardOpen.addEventListener('click', openAddNewCardPopup);
 formElement.addEventListener('submit', handleFormSubmit);
+popupNewPlaceForm.addEventListener('submit', addNewCard);
+
+
+
+
 

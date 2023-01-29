965cf7e8367771277b4e93f0e41fdd519b009d7e
InEmEcI <iikar@bk.ru>
2023-01-29 12:53:12 +0300

    Первые карточки через JS. Удалять их и ставить лайки можно.

---
+++ scripts/cards.js
@@ -0,0 +1,34 @@
+// первые 6 карточек
+const initialCards = [
+
+  {
+    name: 'Калининград',
+    link: './images/kaliningrad.jpg'
+  },
+
+  {
+    name: 'Екатеринбург',
+    link: './images/ekaterinburg.jpg'
+  },
+
+  {
+    name: 'Нижний Новгород',
+    link: './images/nijniy_novgorod.jpeg'
+  },
+
+  {
+    name: 'Казань',
+    link: './images/kazan.jpg'
+  },
+
+  {
+    name: 'Москва',
+    link: './images/moskow.jpg'
+  },
+
+  {
+    name: 'Санкт-Петербург',
+    link: './images/piter.jpg'
+  }
+
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 // const elementItem = document.querySelector('.element');
+
 // кнопка like
-const likeButtons = document.querySelectorAll('.element__like');
+const likeButton = document.querySelector('.element__like');
+
 const popupElement = document.querySelector('.popup');
 const profileOpenButton = document.querySelector('.profile__edit');
 const popupCloseElement = popupElement.querySelector('.popup__close');
@@ -10,31 +12,69 @@ let profileWhoIsThisElement = profileElement.querySelector('.profile__who-is-thi
 let popupNameElement = popupElement.querySelector('.popup__name');
 let popupWhoIsThisElement = popupElement.querySelector('.popup__who-is-this');
 const popupContentSaveButton = popupElement.querySelector('.popup__save');
+const popupAddNewCardSaveButton = popupElement.querySelector('.new-card-save');
 const formElement = document.querySelector('.popup__form-about');
 let nameInput = formElement.querySelector('.popup__input_name_name');
 let jobInput = formElement.querySelector('.popup__input_name_who-is-this');
 
 
+// Карточки с городами
+const cardsSection = document.querySelector('.elements');
+
+// Карточка
+// const cardsSectionElement = cardsSection.querySelector('.element');
+// const cardsSectionElement = document.querySelector('.element');
+
+// изображение в карточке
+// const cardElementImage = cardsSectionElement.querySelector('.element__image').src;
+
+// карточка с городами в template(шаблоне) (<article class="element">)
+const templateCard = document.querySelector('#template-card').content.querySelector('.element');
+// console.log(templateCard);
+
+// кнопка добавления карточки
+const newCardButton = document.querySelector('.profile__add-new');
+
+// кнопка удаления карточки
+const elementTrashButton = cardsSection.querySelector('.element__trash');
+
+// функция для добавления карточки
+function createCard(items) {
+  items.forEach((item) => {
+    const card = templateCard.cloneNode(true);
+    card.querySelector('.element__title').textContent = item.name;
+    card.querySelector('.element__image').alt = item.name;
+    card.querySelector('.element__image').src = item.link;
+    card.querySelector('.element__trash').addEventListener('click', () => card.remove());
+    card.querySelector('.element__like').addEventListener('click',
+      function (event) {
+        event.target.classList.toggle('element__like_active');
+      });
+
+    cardsSection.prepend(card);
+  })
+}
+
+// вставляем карточки из массива initialCards[]
+createCard(initialCards);
+
+// createCard(newCard);
+
+// -------------- УЖЕ БЫЛО ---------------------------
+
+// открытие попапа в профиле
 const popupOpen = function () {
   popupElement.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileWhoIsThisElement.textContent;
 }
 
+// закрытие попапа в профиле
 const popupClose = function () {
   popupElement.classList.remove('popup_opened');
 }
 
-const closePopupByClickOnOverlay = function (event) {
-  console.log(event.target, event.currenTarget);
-}
-
-// const popupVisibility = function () {
-//   popupElement.classList.toggle('popup_opend');
-//   nameInput.value = profileName.textContent;
-//   jobInput.value = profileWhoIsThisElement.textContent;
-// }
-
+// заполнение полей в профиле
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
@@ -42,15 +82,7 @@ function handleFormSubmit(evt) {
   popupClose();
 }
 
-// ставит / убирает  лайки
-// for (let i = 0; i < likeButtons.length; i++) {
-//   likeButtons[i].addEventListener('click', function () {
-//     likeButtons[i].classList.toggle('element__like_active');
-//   })
-// }
-
-
 profileOpenButton.addEventListener('click', popupOpen);
 popupCloseElement.addEventListener('click', popupClose);
 formElement.addEventListener('submit', handleFormSubmit);
-// popupElement.addEventListener('click', closePopupByClickOnOverlay)
+

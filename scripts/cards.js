1709e17bd401ab3d276ccd86495b16a5eb6ad0a0
InEmEcI <iikar@bk.ru>
2023-02-01 14:27:10 +0300

    адаптация картинок при открытии на весь экран 1.4

--- scripts/cards.js
+++ scripts/cards.js
@@ -3,32 +3,32 @@ const initialCards = [
 
   {
     name: 'Калининград',
-    link: './images/kaliningrad.jpg'
+    link: './images/kaliningrad_6.jpg'
   },
 
   {
     name: 'Екатеринбург',
-    link: './images/ekaterinburg.jpg'
+    link: './images/ekatirenburg_5.jpg'
   },
 
   {
     name: 'Нижний Новгород',
-    link: './images/nijniy_novgorod.jpeg'
+    link: './images/nizhnii-novgorod_4.jpg'
   },
 
   {
     name: 'Казань',
-    link: './images/kazan.jpg'
+    link: './images/kazan_3.jpg'
   },
 
   {
     name: 'Москва',
-    link: './images/moskow.jpg'
+    link: './images/moskva_2.jpg'
   },
 
   {
     name: 'Санкт-Петербург',
-    link: './images/piter.jpg'
+    link: './images/piter_1.jpg'
   }
 
 ];
